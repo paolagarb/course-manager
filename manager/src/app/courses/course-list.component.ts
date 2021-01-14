2e4015eae108ef87d32579d7d376f7caa1b09a0b
Paola Garbato <paolagarbato@gmail.com>
2021-01-14 16:02:55 -0300

    service para adicionar lista de cursos, injeção de dependência

--- manager/src/app/courses/course-list.component.ts
+++ manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -10,30 +11,11 @@ export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
 
-    ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular',
-                imageUrl: '/assets/images/Angular.png',
-                price: 129.99,
-                code: 'AS31-12',
-                duration: 240,
-                rating: 4.5,    
-                releaseDate: 'December, 2, 2020'
+    constructor (private courseService: CourseService) {
 
-            },
-            {
-                id: 2,
-                name: 'HTML5',
-                imageUrl: '/assets/images/HTML5.png',
-                price: 29.99,
-                code: 'HS42-10',
-                duration: 120,
-                rating: 4.0,
-                releaseDate: 'November, 7, 2020'
-            },
-        ]
     }
 
+    ngOnInit(): void {
+        this.courses = this.courseService.retrieveAll();
+    }
 }
---
+++ manager/src/app/courses/course.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from "@angular/core";
+import { Course } from "./course";
+
+@Injectable ({
+    providedIn: 'root'
+})
+
+export class CourseService {
+    retrieveAll() : Course[] {
+        return COURSES;
+    }
+}
+
+var COURSES: Course[] = [
+    {
+        id: 1,
+        name: 'Angular',
+        imageUrl: '/assets/images/Angular.png',
+        price: 129.99,
+        code: 'AS31-12',
+        duration: 240,
+        rating: 4.5,    
+        releaseDate: 'December, 2, 2020',
+        description: 'Curso de ANGULAR...'
+    },
+    {
+        id: 2,
+        name: 'HTML5',
+        imageUrl: '/assets/images/HTML5.png',
+        price: 29.99,
+        code: 'HS42-10',
+        duration: 120,
+        rating: 4.0,
+        releaseDate: 'November, 7, 2020',
+        description: 'Curso de HTML5...'
+    },
+    {
+        id: 3,
+        name: 'CSS3',
+        imageUrl: '/assets/images/CSS3.png',
+        price: 39.99,
+        code: 'CGE2-15',
+        duration: 160,
+        rating: 4.5,
+        releaseDate: 'November, 2, 2020',
+        description: 'Curso de CSS3...'
+    },
+    {
+        id: 4,
+        name: 'JavaScript',
+        imageUrl: '/assets/images/JS.png',
+        price: 69.99,
+        code: 'JVN12-30',
+        duration: 240,
+        rating: 5.0,
+        releaseDate: 'November, 11, 2020',
+        description: 'Curso de JavaScript...'
+    },
+    {
+        id: 5,
+        name: 'C#',
+        imageUrl: '/assets/images/Csharp.png',
+        price: 29.99,
+        code: 'HS42-10',
+        duration: 600,
+        rating: 5.0,
+        releaseDate: 'November, 7, 2020',
+        description: 'Curso de C#...'
+    }
+]
