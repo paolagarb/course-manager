f2cb3dae58a4a41a68cd5354c9570b69e764948e
Paola Garbato <paolagarbato@gmail.com>
2021-01-14 14:20:19 -0300

    criando component STAR para formatação das estrelas do curso, e font-awesome para exibição da classificação dos cursos, criando e adicionando folha de estilo para ocultar parte das estrelas

--- manager/src/app/courses/course-list.component.ts
+++ manager/src/app/courses/course-list.component.ts
@@ -19,7 +19,7 @@ export class CourseListComponent implements OnInit {
                 price: 129.99,
                 code: 'AS31-12',
                 duration: 240,
-                rating: 6.2,    
+                rating: 4.5,    
                 releaseDate: 'December, 2, 2020'
 
             },
@@ -30,7 +30,7 @@ export class CourseListComponent implements OnInit {
                 price: 29.99,
                 code: 'HS42-10',
                 duration: 120,
-                rating: 7.0,
+                rating: 4.0,
                 releaseDate: 'November, 7, 2020'
             },
         ]
