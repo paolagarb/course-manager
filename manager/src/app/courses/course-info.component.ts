2bf4d5a68dceeebddeea3d45af2b1808dd92569a
Paola Garbato <paolagarbato@gmail.com>
2021-01-15 13:51:51 -0300

    Adicionando campos para edição, carregando valores em campos e validando se estão vazios

--- manager/src/app/courses/course-info.component.ts
+++ manager/src/app/courses/course-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component ({
     templateUrl: './course-info.component.html'
@@ -7,13 +9,13 @@ import { ActivatedRoute } from "@angular/router";
 
 export class CourseInfoComponent implements OnInit {
 
-    courseId: number;
+    course: Course;
 
-    constructor(private activatedRoute: ActivatedRoute) {
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) {
 
     }
 
     ngOnInit(): void {
-        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'));
     }
 }
